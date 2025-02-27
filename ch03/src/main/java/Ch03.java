2a3270ebf58a7896a701d08d6123ae2cc0bef3c5
TheLeishman <197945159+TheLeishman@users.noreply.github.com>
2025-02-27 15:59:43 -0800

    initial commit

---
+++ ch03/src/main/java/Ch03.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+/**
+ *
+ * @author paul
+ */
+public class Ch03 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
